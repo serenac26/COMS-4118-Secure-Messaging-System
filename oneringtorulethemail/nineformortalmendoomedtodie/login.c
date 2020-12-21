13691ca3a8c5a5643267427d2fe1fec26fd76063
Serena Cheng <serenacheng99@gmail.com>
2020-12-20 19:31:01 -0600

    use relative path in login

--- oneringtorulethemail/nineformortalmendoomedtodie/login.c
+++ oneringtorulethemail/nineformortalmendoomedtodie/login.c
@@ -2,26 +2,22 @@
 #include <string.h>
 #include <stdio.h>
 
-const char *HASHEDPW_PATH = "server/private/credentials";
+const char *HASHEDPW_PATH = "../private/credentials";
 const char *HASHEDPW_SUFFIX = ".hashedpw";
 
 int main(int argc, char *argv[])
 {
-    char cwd[256];
-    getcwd(cwd, sizeof(cwd));
-    printf("%s\n", cwd);
     char *c;
     char *username = argv[1];
     char *pw = argv[2];
-    char *treedir = argv[3];
     char hashedpw_file[100];
     char hashedpw[256];
     FILE *fp;
-    if (argc != 4) {
+    if (argc != 3) {
         fprintf(stderr, "bad arg count; usage: login <username> <password> <treedir>\n");
         return 1;
     }
-    sprintf(hashedpw_file, "%s/%s/%s%s", treedir, HASHEDPW_PATH, username, HASHEDPW_SUFFIX);
+    sprintf(hashedpw_file, "%s/%s%s", HASHEDPW_PATH, username, HASHEDPW_SUFFIX);
     fp = fopen(hashedpw_file, "r");
     if (!fp) {
         printf("%s\n", hashedpw_file);
