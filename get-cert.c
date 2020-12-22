b38a54decd0c52f97cc34f1bbaddf763c18e29d2
hbw2118 <hbw2118@columbia.edu>
2020-12-22 09:42:27 -0500

    adding 4 executables

---
+++ get-cert.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <openssl/ssl.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+
+int main(int argc, char *argv[]) {
+    char *username = getpass("Enter username: ");
+    char *password = getpass("Enter password: ");
+
+    if ((strlen(username) > 32 ) || (strlen(password)>32)) {
+        printf("input too large: must be 32 or less characters\n");
+    }
+
+    char *privatekey = getpass("Enter privatekey: ");
+    SSL_CTX *ctx;
+    SSL *ssl;
+    const SSL_METHOD *meth; 
+    BIO *sbio;
+    int err; char *s;
+
+    int ilen;
+    char ibuf[512];
+    
+    struct sockaddr_in sin;
+    int sock;
+    struct hostent *he;
+    SSL_library_init();
+    SSL_load_error_strings();
+
+    meth = TLS_client_method();
+	ctx = SSL_CTX_new(meth);
+	SSL_CTX_set_default_verify_dir(ctx);
+	SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
+
+	ssl = SSL_new(ctx);
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock < 0) {
+		perror("socket");
+		return 1;
+	}
+
+    bzero(&sin, sizeof sin);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(443);
+	he = gethostbyname("");//edit this
+
+	memcpy(&sin.sin_addr, (struct in_addr *)he->h_addr, he->h_length);
+	if (connect(sock, (struct sockaddr *)&sin, sizeof sin) < 0) {
+		perror("connect");
+		return 2;
+	}
+	sbio=BIO_new(BIO_s_socket());
+	BIO_set_fd(sbio, sock, BIO_NOCLOSE);
+	SSL_set_bio(ssl, sbio, sbio);
+	err = SSL_connect(ssl);
+
+	if (SSL_connect(ssl) != 1) {
+		switch (SSL_get_error(ssl, err)) {
+			case SSL_ERROR_NONE: s="SSL_ERROR_NONE"; break;
+			case SSL_ERROR_ZERO_RETURN: s="SSL_ERROR_ZERO_RETURN"; break;
+			case SSL_ERROR_WANT_READ: s="SSL_ERROR_WANT_READ"; break;
+			case SSL_ERROR_WANT_WRITE: s="SSL_ERROR_WANT_WRITE"; break;
+			case SSL_ERROR_WANT_CONNECT: s="SSL_ERROR_WANT_CONNECT"; break;
+			case SSL_ERROR_WANT_ACCEPT: s="SSL_ERROR_WANT_ACCEPT"; break;
+			case SSL_ERROR_WANT_X509_LOOKUP: s="SSL_ERROR_WANT_X509_LOOKUP"; break;
+			case SSL_ERROR_WANT_ASYNC: s="SSL_ERROR_WANT_ASYNC"; break;
+			case SSL_ERROR_WANT_ASYNC_JOB: s="SSL_ERROR_WANT_ASYNC_JOB"; break;
+			case SSL_ERROR_SYSCALL: s="SSL_ERROR_SYSCALL"; break;
+			case SSL_ERROR_SSL: s="SSL_ERROR_SSL"; break;
+		}
+		fprintf(stderr, "SSL error: %s\n", s);
+		ERR_print_errors_fp(stderr);
+		return 3;
+	}
+
+    //writing stuff with http
+    //GET /HTTP/1.0
+
+    return 0;
+}
