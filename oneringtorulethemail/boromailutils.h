a8dc65c79b3edbf264d0b543023c5d2f2ca1b9e7
Serena Cheng <serenacheng99@gmail.com>
2020-12-24 00:34:17 -0600

    update boromailutils header

--- oneringtorulethemail/boromailutils.h
+++ oneringtorulethemail/boromailutils.h
@@ -1,7 +1,7 @@
 #ifndef __BOROMAILUTILS_H__
 #define __BOROMAILUTILS_H__
 
-struct Node *getrecipientcerts(struct Node *recipients);
+int getrecipientcert(char *cert, bstring recipient);
 
 int sendmsg(bstring sender, struct Node *recipient, struct Node *recipients, bstring msgin);
 
