173fdce3a9cb1d22b3123969fd8947d2126159e2
Serena Cheng <serenacheng99@gmail.com>
2020-12-25 00:41:23 -0600

    fix maketestcert

--- maketestcert.sh
+++ maketestcert.sh
@@ -3,10 +3,10 @@
 # uncomment main in faramailutils.c first
 make faramailutils TREE=$1
 cd $1/client/bin
-./genkey.sh ../../../$2.key.pem $4
+./genkey.sh ../../../$2.key.pem $3
 ./makecsr.sh ../imopenssl.cnf $2 ../../../$2.key.pem ../../../$2.req.pem
 cd ../../..
 sudo cp $2.req.pem $1/server/ca/intermediate/csr/
 cd $1/server/bin
 sudo ./faramailutils getcert $2
-sudo cp ../ca/intermediate/certs/$2.cert.pem $3
+sudo cp ../ca/intermediate/certs/$2.cert.pem ../../../$2.cert.pem
